96ac1e05bbc055dbd33b36c0a4fdfe4420b23caf
Khudenko Yulia <yuliakhudenko6@gmail.com>
2026-04-24 16:28:56 +0300

    chore: keep only README on main

--- server/src/presentation/routes/health.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const healthController = require('../controllers/healthController');
-
-// Оскільки ми підключимо цей роутер на префікс '/health',
-// тут достатньо вказати просто '/'
-router.get('/', healthController.checkHealth);
-
-module.exports = router;
