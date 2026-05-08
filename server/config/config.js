34464098ae5970a1b4498b3402260b3ea3d44ff4
Khudenko Yulia <yuliakhudenko6@gmail.com>
2026-05-08 14:29:15 +0300

    fix: migrate sequelize config to config.js with env variables

--- server/config/config.js
+++ server/config/config.js
@@ -6,4 +6,11 @@ module.exports = {
     host: process.env.DB_HOST || 'db',
     dialect: 'mysql',
   },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST || 'db',
+    dialect: 'mysql',
+  },
 };
